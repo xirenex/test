7324349e655c2dae45ce790b9e9235f0f661f87c
xirenex <62133453+xirenex@users.noreply.github.com>
2020-03-27 16:02:09 +0800

    Add files via upload

---
+++ module/func.py
@@ -0,0 +1,91 @@
+from django.conf import settings
+
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+
+def sendText(event):  #傳送文字
+    try:
+        message = TextSendMessage(  
+            text = "我是 Linebot，\n您好！"
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendImage(event):  #傳送圖片
+    try:
+        message = ImageSendMessage(
+            original_content_url = "https://i.imgur.com/4QfKuz1.png",
+            preview_image_url = "https://i.imgur.com/4QfKuz1.png"
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendStick(event):  #傳送貼圖
+    try:
+        message = StickerSendMessage(  #貼圖兩個id需查表
+            package_id='1',  
+            sticker_id='2'
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendMulti(event):  #多項傳送
+    try:
+        message = [  #串列
+            StickerSendMessage(  #傳送貼圖
+                package_id='1',  
+                sticker_id='2'
+            ),
+            TextSendMessage(  #傳送y文字
+                text = "這是 Pizza 圖片！"
+            ),
+            ImageSendMessage(  #傳送圖片
+                original_content_url = "https://i.imgur.com/4QfKuz1.png",
+                preview_image_url = "https://i.imgur.com/4QfKuz1.png"
+            )
+        ]
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendPosition(event):  #傳送位置
+    try:
+        message = LocationSendMessage(
+            title='101大樓',
+            address='台北市信義路五段7號',
+            latitude=25.034207,  #緯度
+            longitude=121.564590  #經度
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendQuickreply(event):  #快速選單
+    try:
+        message = TextSendMessage(
+            text='請選擇最喜歡的程式語言',
+            quick_reply=QuickReply(
+                items=[
+                    QuickReplyButton(
+                        action=MessageAction(label="Python", text="Python")
+                    ),
+                    QuickReplyButton(
+                        action=MessageAction(label="Java", text="Java")
+                    ),
+                    QuickReplyButton(
+                        action=MessageAction(label="C#", text="C#")
+                    ),
+                    QuickReplyButton(
+                        action=MessageAction(label="Basic", text="Basic")
+                    ),
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
