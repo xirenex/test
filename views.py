0a3a5ba927ceb4e0341a4474d5227824f2b76656
xirenex <62133453+xirenex@users.noreply.github.com>
2020-03-27 16:25:48 +0800

    Add files via upload

---
+++ views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+
+def sayhello(request):
+   return HttpResponse("Hello Django!")
+
+def hello2(request,username):
+   return HttpResponse("Hello " + username)
+   
+def hello3(request,username):
+   now=datetime.now()
+   return render(request,"hello3.html",locals())
+   
+def hello4(request,username):
+   now=datetime.now()
+   return render(request,"hello4.html",locals())
